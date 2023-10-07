ddb5cf724c9a0b5a0a60e8b572531bc3cce3d3b4
Janett123 <masiko@centrum.cz>
2023-10-07 21:25:30 +0200

    Použití direktivy include

--- src/main/java/cz/czechitas/java2webapps/lekce3/controller/DateTimeController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce3/controller/DateTimeController.java
@@ -2,6 +2,7 @@ package cz.czechitas.java2webapps.lekce3.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
@@ -40,8 +41,11 @@ public class DateTimeController {
    * @return Sdružený objekt obsahující model a informace o view.
    */
   @GetMapping("/datum")
-  public String datum() {
-    return "index";
+  public ModelAndView  datum() {
+ ModelAndView result = new ModelAndView("datum");
+      result.addObject("datum",LocalDate.now().format(DATE_FORMATTER));
+ return result;
+
   }
 
   /**
@@ -50,7 +54,9 @@ public class DateTimeController {
    * @return Sdružený objekt obsahující model a informace o view.
    */
   @GetMapping("/cas")
-  public String cas() {
-    return "index";
+  public ModelAndView  cas() {
+    ModelAndView result = new ModelAndView("cas");
+    result.addObject("cas",LocalTime.now().format(TIME_FORMATTER));
+    return result;
   }
 }
